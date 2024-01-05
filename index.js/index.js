f9df58b27099fc36b31876d7e6e053ba957c8ca4
kaziarman23 <kaziarman@proton.me>
2024-01-05 10:34:52 +0600

    first-commit

---
+++ index.js/index.js
@@ -0,0 +1,28 @@
+function getComputerChoice(){
+   console.log("rock");
+    return
+}
+
+getComputerChoice();
+
+
+
+function getplayerChoice(move){
+    if(move = "rock"){
+        console.log("rock");
+        return
+    }else if(move ="paper"){
+        console.log("paper");
+        return 
+    }else if(move = "scissore"){
+        console.log("scissore");
+        return
+    }
+    
+}
+getplayerChoice("");
+
+
+    
+
+
