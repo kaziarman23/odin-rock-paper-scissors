6419b7f270a42676d30dc1827eaf0d545a005765
kaziarman23 <kaziarman@proton.me>
2024-01-17 13:25:04 +0600

    final_project_done_By_Me

--- js/index.js
+++ js/index.js
@@ -1,63 +1,88 @@
-function game() {
-  for (let i = 1; i <= 5; i++) {
-    playRound();
+const winners =[];
+
+function game(){
+  for(let i = 1; i <= 5; i++){
+    playRound(i)
   }
+}
+
+function playRound(round){
+    const playerSelection = getplayerchoice()
+    const computerSelection = getcomputerchoice()
+    const winner =winnerCheck(playerSelection,computerSelection)
+    winners.push(winner);
+    logRound(getplayerchoice,getcomputerchoice,winner,round)
+}
+
+function getplayerchoice(){
+  let input =prompt("Type Rock,Paper,Scissor")
+  while(input === null){
+    input = prompt("Please try to understand, Type Rock,Paper,Scissor only")
+  }
+  input.toLocaleLowerCase();
   
+  if(input === "rock"){
+    console.log("playerGive:","rock")
+  }else if(input === "paper"){
+    console.log("playerGive:","paper")
+  }else if(input === "scissor"){
+    console.log("playerGive:","scissor")
+  }else{
+    console.log("ahaa dude typing mistake, Try Again")
+  }
+  return input
 }
 
-function playRound() {
-  const playerSelection = getPlayerChoice();
-  const computerSelection = getComputerChoice();
-  const winner = winnerCheck(playerSelection, computerSelection);
-  console.log(winner)
-  
-}
+function getcomputerchoice(){
+  let random =Math.ceil(Math.random()*3)
 
-function getComputerChoice() {
-  let random = Math.ceil(Math.random() * 3);
-  if (random === 1) {
-    console.log("ComputerGive:", "rock");
-    return "rock";
-  } else if (random === 2) {
-    console.log("ComputerGive:", "paper");
-    return "paper";
-  } else {
-    console.log("ComputerGive:", "scissor");
-    return "scissor";
+  if(random === 1){
+    console.log("computerGive:","rock")
+    return "rock"
+  }else if(random === 2){
+    console.log("computerGive:","paper")
+    return "paper"
+  }else{
+    console.log("computerGive:","scissor")
+    return "scissor"
   }
 }
 
-function getPlayerChoice() {
-  let input = prompt("Type rock, paper, scissor");
-  while (input === null) {
-    input = prompt("Please type rock, paper, scissor");
-  }
-  input = input.toLowerCase();
 
-  if (input === "rock") {
-    console.log("playerGive:", "rock");
-  } else if (input === "paper") {
-    console.log("playerGive:", "paper");
-  } else if (input === "scissor") {
-    console.log("playerGive:", "scissor");
-  } else {
-    console.log("ahaaa typing mistake, try again");
-  }
-  return input;
-}
+function winnerCheck(choiceP,choiceC){
 
-function winnerCheck(choiceP, choiceC) {
-  if (choiceP === choiceC) {
-    return "Round_Tie";
-  } else if (
-    (choiceP === "rock" && choiceC === "scissor") ||
-    (choiceP === "paper" && choiceC === "rock") ||
-    (choiceP === "scissor" && choiceC === "paper")
-  ) {
-    return "player win the Round";
-  } else {
-    return "computer win the Round";
+  if(choiceP === choiceC){
+    return "Round Tie"
+  }
+  else if(
+            (choiceP === "rock" && choiceP === "scissor")||
+            (choiceP === "paper" && choiceP === "rock")||
+            (choiceP === "scissor" && choiceP === "paper")
+          )
+  {       
+    return "player win the Round"
+  }
+  else{
+    return "computer win the Round"
   }
 }
 
-game();
+
+function logRound(getplayerchoice,getcomputerchoice,winner,round){
+  let pwin =winners.filter(function (item){
+    return item === "player win the Round"
+  }).length
+  let cwin =winners.filter(function (item){
+    return item === "computer win the Round"
+  }).length
+  let tie =winners.filter(function (item){
+    return item === "Round Tie"
+  }).length
+  console.log("Round",round)
+  console.log("player score:",pwin)
+  console.log("computer score:",cwin)
+  console.log("Tie round:",tie)
+  console.log("........................")
+}
+
+game()
