fb08346233773d8b9a92a97c1a22e8effe6c1025
kaziarman23 <kaziarman@proton.me>
2024-01-20 11:49:06 +0600

    projetDone

--- js/index.js
+++ js/index.js
@@ -2,13 +2,15 @@ const buttonRock = document.querySelector(".rock")
 const buttonPaper = document.querySelector(".paper")
 const buttonScissor = document.querySelector(".scissor")
 const roundResult = document.querySelector(".outcome")
-  
 
+let playerScore = 0;
+let computerScore = 0;
 
 buttonRock.addEventListener("click",function(){
   const computerSelection = getcomputerchoice()
   const playerSelection ="rock"
   playRound(playerSelection,computerSelection)
+  
 })
 buttonPaper.addEventListener("click",function(){
   const computerSelection = getcomputerchoice()
@@ -54,11 +56,43 @@ function playRound(choiceP,choiceC){
     const p = document.createElement("p")
     p.innerText = "Player win the Round"
     roundResult.append(p)
+    playerScore++
   }
   else{      
     const p = document.createElement("p")
     p.innerText = "Computer win the Round"
     roundResult.append(p)
+    computerScore++
   }
+
+  displayScore()
+  cheakWinner()
 }
 
+
+function displayScore(){
+    const score =document.querySelector(".score")
+    score.innerText =`Player score: ${playerScore} ,Computer score: ${computerScore}`
+}
+
+
+function cheakWinner(){
+    if( playerScore === 5){
+        const h2 =document.createElement("h2")
+        h2.innerText ="player win the match, Thanks for playing"
+        roundResult.appendChild(h2)
+        disableButtons()
+    }
+    else if( computerScore === 5){
+        const h2 =document.createElement("h2")
+        h2.innerText ="computer win the match, Thanks for playing"
+        roundResult.appendChild(h2)
+        disableButtons()
+    }
+}
+
+function disableButtons() {
+    buttonRock.disabled = true;
+    buttonPaper.disabled = true;
+    buttonScissor.disabled = true;
+  }
