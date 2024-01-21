1cc5ff17c1ca4ae04fb70362546fc75b9ed8a7e0
kaziarman23 <kaziarman@proton.me>
2024-01-21 09:58:42 +0600

    projectReady

--- js/index.js
+++ js/index.js
@@ -1,98 +1,101 @@
-const buttonRock = document.querySelector(".rock")
-const buttonPaper = document.querySelector(".paper")
-const buttonScissor = document.querySelector(".scissor")
-const roundResult = document.querySelector(".outcome")
-
 let playerScore = 0;
 let computerScore = 0;
 
-buttonRock.addEventListener("click",function(){
-  const computerSelection = getcomputerchoice()
-  const playerSelection ="rock"
-  playRound(playerSelection,computerSelection)
-  
+const rockButton = document.querySelector(".rock")
+const paperButton = document.querySelector(".paper")
+const scissorButton = document.querySelector(".scissor")
+const outcomeDiv = document.querySelector(".outcome")
+
+
+rockButton.addEventListener("click",function (){
+    const computerSelection = getcomputerchoice()
+    const playerSelection = "rock"
+    playRound(computerSelection,playerSelection)
 })
-buttonPaper.addEventListener("click",function(){
-  const computerSelection = getcomputerchoice()
-  const playerSelection ="paper"
-  playRound(playerSelection,computerSelection)
+
+paperButton.addEventListener("click",function (){
+    const computerSelection = getcomputerchoice()
+    const playerSelection = "paper"
+    playRound(computerSelection,playerSelection)
 })
-buttonScissor.addEventListener("click",function(){
-  const computerSelection = getcomputerchoice()
-  const playerSelection ="scissor"
-  playRound(playerSelection,computerSelection)
+
+scissorButton.addEventListener("click",function (){
+    const computerSelection = getcomputerchoice()
+    const playerSelection = "scissor"
+    playRound(computerSelection,playerSelection)
 })
 
 
 function getcomputerchoice(){
-  let random =Math.ceil(Math.random()*3)
+  let random = Math.ceil(Math.random()*3)
 
   if(random === 1){
-    console.log("computerGive:","rock")
+    console.log("ComputerGive:","rock")
     return "rock"
-  }else if(random === 2){
-    console.log("computerGive:","paper")
+  }
+  else if(random === 2){
+    console.log("ComputerGive:","paper")
     return "paper"
-  }else{
-    console.log("computerGive:","scissor")
+  }
+  else{
+    console.log("ComputerGive:","scissor")
     return "scissor"
   }
 }
 
 
 function playRound(choiceP,choiceC){
-  
+   
   if(choiceP === choiceC){
-    const p = document.createElement("p")
+    let p = document.createElement("p")
     p.innerText = "Round Tie"
-    roundResult.append(p)
+    outcomeDiv.append(p)
   }
   else if(
-            (choiceP === "rock" && choiceC === "scissor")||
-            (choiceP === "paper" && choiceC === "rock")||
-            (choiceP === "scissor" && choiceC === "paper")
-          )
-  {       
-    const p = document.createElement("p")
-    p.innerText = "Player win the Round"
-    roundResult.append(p)
+          (choiceP === "rock" && choiceC === "scissor")||   
+          (choiceP === "paper" && choiceC === "rock")||   
+          (choiceP === "scissor" && choiceC === "paper")   
+         )
+  {
+    let p = document.createElement("p")
+    p.innerText = "Player win the match"
+    outcomeDiv.append(p)
     playerScore++
   }
-  else{      
-    const p = document.createElement("p")
-    p.innerText = "Computer win the Round"
-    roundResult.append(p)
+  else{
+    let p = document.createElement("p")
+    p.innerText = "Computer win the match"
+    outcomeDiv.append(p)
     computerScore++
   }
-
-  displayScore()
+  displayScore ()
   checkWinner()
 }
 
 
-function displayScore(){
-    const score =document.querySelector(".score")
-    score.innerText =`Player score: ${playerScore} ,Computer score: ${computerScore}`
+function displayScore (){
+  const score = document.querySelector(".score")
+  score.innerText = `player score: ${playerScore} ,Computer score: ${computerScore}`
 }
-
 
 function checkWinner(){
-    if( playerScore === 5){
-        const h2 =document.createElement("h2")
-        h2.innerText ="player win the match, Thanks for playing"
-        roundResult.appendChild(h2)
-        disableButtons()
-    }
-    else if( computerScore === 5){
-        const h2 =document.createElement("h2")
-        h2.innerText ="computer win the match, Thanks for playing"
-        roundResult.appendChild(h2)
-        disableButtons()
-    }
+  if(playerScore === 5){
+    let h2 =document.createElement("h2")
+    h2.innerText = "Player win the match"
+    outcomeDiv.append(h2)
+    disableButtons()
+  }
+  else if(computerScore === 5){
+    let h2 =document.createElement("h2")
+    h2.innerText = "Computer win the match"
+    outcomeDiv.append(h2)
+    disableButtons()
+  }
 }
 
-function disableButtons() {
-    buttonRock.disabled = true;
-    buttonPaper.disabled = true;
-    buttonScissor.disabled = true;
-  }
+
+function disableButtons(){
+  rockButton.disabled =true
+  paperButton.disabled =true
+  scissorButton.disabled =true
+}
