625a2e3cb8dc109798b31909c8a15d9f7183a857
kaziarman23 <kaziarman@proton.me>
2024-02-22 15:58:05 +0600

    recreating-project

--- js/index.js
+++ js/index.js
@@ -1,88 +1,69 @@
-function getComputerChoice(){
-    const getNUM = Math.ceil(Math.random() * 3);
-    if(getNUM === 1){
-      console.log("ComputerGive:", "rock");
-      return "rock";
-    } else if(getNUM === 2){
-      console.log("ComputerGive:", "paper");
-      return "paper";
-    } else {
-      console.log("ComputerGive:", "scissor");
-      return "scissor";
-    }
+const rock = document.querySelector(".rock")
+const paper = document.querySelector(".paper")
+const scissor = document.querySelector(".scissor")
+const score = document.querySelector(".scoer")
+const declaretion = document.querySelector(".declaretion")
+
+let playerScore = 0;
+let computerScore = 0;
+let winner =[];
+
+rock.addEventListener("click",function(){
+  const computerSecletion = getcomputerchoice()
+  const playerSecletion = "rock"
+  winnerCheck("playerSecletion","computerSecletion")
+  // winner.push(winnerCheck)
+
+})
+
+paper.addEventListener("click",function(){
+  const computerSecletion = getcomputerchoice()
+  const playerSecletion = "paper"
+  winnerCheck("playerSecletion","computerSecletion")
+  // winner.push(winnerCheck)
+})
+
+scissor.addEventListener("click",function(){
+  const computerSecletion = getcomputerchoice()
+  const playerSecletion = "scissor"
+  winnerCheck("playerSecletion","computerSecletion")
+  // winner.push(winnerCheck)
+})
+
+function getcomputerchoice(){
+  const computerGive = Math.ceil(Math.random()*3)
+
+  if(computerGive === 1){
+    return "rock"
   }
-  
-  function getPlayerChoice(){
-    let input = prompt("Type Rock, Paper, Scissor");
-    while(input === null){
-      input = prompt("Please try to understand. Type Rock, Paper, Scissor");
-    }
-    input = input.toLowerCase();
-    if(input === "rock"){
-      console.log("playerGive:", "rock");
-    } else if(input === "paper"){
-      console.log("playerGive:", "paper");
-    } else if(input === "scissor"){
-      console.log("playerGive:", "scissor");
-    } else {
-      console.log("uff typing mistake, please Try Again");
-    }
-    return input;
+  else if(computerGive === 2){
+    return "paper"
   }
-  
-  const winners = [];
-  
-  function winnerCheck(choiceP, choiceC){
-    if(choiceP === choiceC){
-      return "Round Tie";
-    } else if(
-      (choiceP === "rock" && choiceC === "scissor") ||
-      (choiceP === "paper" && choiceC === "rock") ||
-      (choiceP === "scissor" && choiceC === "paper")
-    ){
-      return "Player win the round";
-    } else {
-      return "Computer win the round";
-    }
+  else{
+    return "scissor"
   }
-  
-  function logwin(){
-    let playerWins = winners.filter(function(item){
-      return item === "Player win the round"
-    }).length
-    let computerWins = winners.filter(function(item){
-      return item === "Computer win the round"
-    }).length
-    let Ties = winners.filter(function(item){
-      return item === "Round Tie"
-    }).length
-    console.log("Results:");
-    console.log("player Wins:", playerWins);
-    console.log("Computer Wins:", computerWins);
-    console.log("Ties:", Ties);
+}
+
+function winnerCheck(choiceP , choiceC){
+  if (choiceP === choiceC){
+    let comment = document.createElement("p")
+    p.innerHTML =`<p></p>`
   }
-  
-  function logRounds(getPlayerChoice, getComputerChoice, winner, round){
-    console.log("Round:", round);
-    console.log("player chose:", getPlayerChoice);
-    console.log("computer chose:", getComputerChoice);
-    console.log(winner);
-    console.log("....................");
+  else if (
+    (choiceP === "rock" && choiceC === "scissor")||
+    (choiceP === "paper" && choiceC === "rock")||
+    (choiceP === "scissor" && choiceC === "paper")
+  ){
+    score.innerHTML = `player win the Round`
+    playerScore++
   }
-  
-  function playRound(round){
-    const playerSelection = getPlayerChoice();
-    const computerSelection = getComputerChoice();
-    const winner = winnerCheck(playerSelection, computerSelection);
-    winners.push(winner);
-    logRounds(playerSelection, computerSelection, winner, round);
+  else{
+    score.innerHTML = `computer win the Round`
+    computerScore++
   }
-  
-  function game(){
-    for(let i = 1; i <= 5; i++){
-      playRound(i);
-    }
-    logwin();
-  }
-  
-  game();
+}
+
+
+function roundScore(){
+  score.innerHTML =`player score:${playerScore} ,computer score:${computerScore}` 
+}
